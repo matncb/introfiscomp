4cf249bf778655dbc6abc96d3613fe8de3423401
matncb <matheus.brustelo@gmail.com>
2025-09-25 20:31:19 -0300

    dois primeiros exercícios

--- P2/exer1.f90
+++ P2/exer1.f90
@@ -1,20 +1,23 @@
-program exer1
-    implicit none
+! Modulo para o cálculo a partir das funções analíticas
 
+module precision
+    implicit none
     integer, parameter :: sp = kind(0.e0)
     integer, parameter :: dp = kind(0.d0)
-    integer, parameter :: p = dp
+    integer, parameter :: p = dp ! Define precisão padrão
 
-    real(p), parameter :: x0 = 0.5
-    real(p), parameter :: h_list(14) = (/0.5,0.2,0.1,0.05,0.01, &
-                            & 0.005,0.001, 0.0005,0.0001, 0.00005, 0.00001, &
-                            & 0.000001, 0.0000001, 0.00000001 /)
+    private
+    public :: sp, dp, p
+end module precision
 
-    call table(x0, h_list)
+module func
+    use precision
+    implicit none
 
+    private
+    public :: f, df, ddf, dddf
 contains
-    ! Carregar entrada
-    ! Funções teóricas
+
     function f(x) result(res)
         real(p) :: x
         real(p) :: res
@@ -30,17 +33,28 @@ contains
     function ddf(x) result(res)
         real(p) :: x
         real(p) :: res
-        res = 2/((1 + x**2)**2)
+        res = 2*(1-x**2)/((1 + x**2)**2)
     end function ddf
 
     function dddf(x) result(res)
         real(p) :: x
         real(p) :: res
-        res = -8*x/((1 + x**2)**3)
+        res = (4*x**3-12*x)/((1 + x**2)**3)
     end function dddf
 
-    ! Discretização
+end module func
 
+module derivatives
+    use precision
+    use func
+    implicit none
+
+    private
+    public :: d_frente_2, d_tras_2, d_sim_3, d_sim_5, dd_sim_3, dd_sim_5, ddd_antisim_5
+
+contains
+
+    ! Lida com a discretização
     function fn(x0,h,n) result(res)
         real(p) :: x0
         real(p) :: h
@@ -51,54 +65,55 @@ contains
         res = f(x0 + h*n)
     end function fn
 
-    ! Derivadas seguindo a ordem do pdf
+    ! Derivadas pedidas no pdf
+    ! Código: d_(frente/tras/sim)_(n_pontos)
 
-    function d1(x0,h) result(res)
+    function d_frente_2(x0,h) result(res)
         real(p) :: x0
         real(p) :: h
 
         real(p) :: res
 
         res = (fn(x0, h, 1) - fn(x0, h, 0))/h
-    end function d1
+    end function d_frente_2
 
-    function d2(x0,h) result(res)
+    function d_tras_2(x0,h) result(res)
         real(p) :: x0
         real(p) :: h
 
         real(p) :: res
 
         res = (fn(x0, h, 0) - fn(x0, h, -1))/h
-    end function d2
+    end function d_tras_2
 
-    function d3(x0,h) result(res)
+    function d_sim_3(x0,h) result(res)
         real(p) :: x0
         real(p) :: h
 
         real(p) :: res
 
         res = (fn(x0, h, 1) - fn(x0, h, -1))/(2*h)
-    end function d3
+    end function d_sim_3
 
-    function d4(x0,h) result(res)
+    function d_sim_5(x0,h) result(res)
         real(p) :: x0
         real(p) :: h
 
         real(p) :: res
 
         res = (-fn(x0, h, 2) + 8*fn(x0, h, 1) -8*fn(x0, h, -1) + fn(x0, h, -2)) /(12*h)
-    end function d4
+    end function d_sim_5
 
-    function d5(x0,h) result(res)
+    function dd_sim_3(x0,h) result(res)
         real(p) :: x0
         real(p) :: h
 
         real(p) :: res
 
         res = (fn(x0, h, 1) -2*fn(x0, h, 0) + fn(x0, h, -1) )/(h**2)
-    end function d5
+    end function dd_sim_3
 
-    function d6(x0,h) result(res)
+    function dd_sim_5(x0,h) result(res)
         real(p) :: x0
         real(p) :: h
 
@@ -106,37 +121,131 @@ contains
 
         res = (-fn(x0, h, 2) +16*fn(x0, h, 1) - 30*fn(x0, h, 0) &
              &+16*fn(x0,h,-1) - fn(x0,h, -2))/(12*h**2)
-    end function d6
+    end function dd_sim_5
 
-
-    function d7(x0,h) result(res)
+    function ddd_antisim_5(x0,h) result(res)
         real(p) :: x0
         real(p) :: h
 
         real(p) :: res
 
         res = (fn(x0, h, 2) -2*fn(x0, h, 1) + 2*fn(x0, h, -1) - fn(x0,h,-2) )/(2*h**3)
-    end function d7
-    
-    ! Elaborar tabela
+    end function ddd_antisim_5 
 
-    subroutine table(x0, h_list)
-        real(p):: x0
-        real(p):: h_list(14)
-        integer :: i
+end module derivatives
 
-        open(unit=1, file='tab1_out.dat', status='replace') 
+module data_handler
+    use precision
+    use derivatives
+    use func
+    implicit none
 
-        write (1,*) "h ", "derivada simétrica 3 pontos ", "derivada para frente 2 pontos ", &
-                &"derivada para trás 2 pontos ", "derivada segunda simétrica 3 pontos ", &
-                & "derivada segunda simétrica 5 pontos ", "derivada terceira anti-simétrica 5 pontos"
+    contains
 
-        do i = 1, 14
-            write (1,*) h_list(i), d3(x0,h_list(i)), d1(x0,h_list(i)), d2(x0,h_list(i)),&
-             & d5(x0,h_list(i)), d6(x0,h_list(i)), d7(x0,h_list(i))
+    subroutine load_tab1_in(h_num, h_array)
+        integer, intent(out) :: h_num
+        real(p), allocatable, intent(out) :: h_array(:)
+
+        open(unit=1, file= "tab1_in.dat", status ="old", action = "read" )
+        read(1, *) h_num
+
+        allocate(h_array(h_num))
+        read (1, *) h_array
+
+        close(1)
+    end subroutine load_tab1_in
+
+    subroutine gen_out(x0, h_num, h_array)
+        real(p), intent(in) :: x0
+        integer, intent(in) :: h_num
+        real(p), allocatable, intent(in) :: h_array(:)
+
+        real(p), allocatable :: matrix_out(:,:)
+        integer :: i, min_ind
+        real(p) :: min_val
+        integer :: min_ind_vec(1)
+
+        character(len = 50) :: label(7)
+        label(1) = "h"
+        label(2) = "Derivada simétrica 3 pontos"
+        label(3) = "Derivada para frente 2 pontos"
+        label(4) = "Derivada para trás 2 pontos"
+        label(5) = "Derivada segunda simétrica 3 pontos"
+        label(6) = "Derivada segunda simétrica 5 pontos"
+        label(7) = "Derivada terceira anti-simétrica 5 pontos"
+        
+        ! Derivada simétrica de 5 pontos não é usada msm?
+
+        allocate(matrix_out(h_num, 6))
+
+        do i = 1, h_num
+           matrix_out(i, 1) = d_sim_3(x0,h_array(i))
+           matrix_out(i, 2) = d_frente_2(x0,h_array(i))
+           matrix_out(i, 3) = d_tras_2(x0,h_array(i))
+           matrix_out(i, 4) = dd_sim_3(x0,h_array(i))
+           matrix_out(i, 5) = dd_sim_5(x0,h_array(i))
+           matrix_out(i, 6) = ddd_antisim_5(x0,h_array(i))
         end do
 
-        close(unit = 1)
-    end subroutine table
+        ! Calcular diferença do valor teórico
+        matrix_out = transpose(matrix_out) ! h_num x 6 -> 6 x h_num
+        matrix_out(1, :) = abs(matrix_out(1,:) - df(x0))
+        matrix_out(2, :) = abs(matrix_out(2,:) - df(x0))
+        matrix_out(3, :) = abs(matrix_out(3,:) - df(x0))
+        matrix_out(4, :) = abs(matrix_out(4,:) - ddf(x0))
+        matrix_out(5, :) = abs(matrix_out(5,:) - ddf(x0))
+        matrix_out(6, :) = abs(matrix_out(6,:) - dddf(x0))
 
+        print *, "Melhores valores de h: "
+        print *, ""
+
+        do i = 1, 6
+            min_val = minval(matrix_out(i, :))
+            min_ind_vec = minloc(matrix_out(i, :))
+            min_ind = min_ind_vec(1)
+
+            print *, trim(label(i+1)), " :"
+            write(*,"(A)",advance = "no") " h = "
+            write(*,"(ES8.1)") h_array(min_ind) 
+            print *, "Diferença de ", min_val
+            print *, ""
+        end do
+
+        matrix_out = transpose(matrix_out)
+
+        open(unit=1, file="tab1_out.dat", status="replace", action = "write") 
+
+        do i = 1, 7
+            write(1, '(A, A)', advance='no') trim(label(i)), '   '
+        end do
+        write(1, *) 
+            
+        do i = 1, h_num
+            write(1,"(ES8.1)", advance = "no") h_array(i)
+            write(1, *) matrix_out(i, :)
+        end do
+
+        close(1)
+
+        deallocate(matrix_out)
+    end subroutine gen_out
+
+end module data_handler
+
+
+program exer1
+    use precision
+    use data_handler
+    implicit none
+
+    real(p), parameter :: x0 = 0.5
+
+    integer :: h_num
+    real(p), allocatable:: h_array(:)
+
+    call load_tab1_in(h_num, h_array)
+    call gen_out(x0, h_num, h_array)
+
+    deallocate(h_array)
+    
 end program exer1
---
+++ P2/exer2.f90
@@ -0,0 +1,219 @@
+! Modulo para o cálculo a partir das funções analíticas
+
+module parametros
+    implicit none
+    integer, parameter :: sp = kind(0.e0)
+    integer, parameter :: dp = kind(0.d0)
+    integer, parameter :: p = dp ! Define precisão padrão
+
+    real(p), parameter :: a = 0., b = 1.
+
+    private
+    public :: sp, dp, p, a, b
+end module parametros
+
+module func
+    use parametros
+    implicit none
+
+    private
+    public :: f, f_primitiva, int_f
+contains
+
+    function f(x) result(res)
+        real(p) :: x
+        real(p) :: res
+        res = x**2 * cos(x)
+    end function f
+
+    function f_primitiva(x) result(res)
+        real(p) :: x
+        real(p) :: res
+        res = x**2 * sin(x) + 2*x*cos(x) -2*sin(x)
+    end function f_primitiva
+
+    function int_f() result(res)
+        real(p):: res
+        res = f_primitiva(b) - f_primitiva(a)
+    end function int_f
+
+end module func
+
+module integrals
+    use parametros
+    use func
+    implicit none
+
+contains
+    ! Lida com a discretização
+    function fn(x0,h,n) result(res)
+        real(p) :: x0
+        real(p) :: h
+        integer :: n
+
+        real(p) :: res
+
+        res = f(x0 + h*n)
+    end function fn
+
+    function find_h(N) result(res)
+        integer :: N
+        real(p) :: res
+        res = (b-a)/N
+    end function find_h
+
+    function trapezio(N) result(res)
+        real(p) :: res, h
+        integer :: N, i
+        real(p) :: x0
+
+        res = 0
+        h = find_h(N)
+        x0 = a + h
+
+        do i = 1, N
+            res = res + h/2 * (fn(x0,h,1) + 2*fn(x0,h,0) + fn(x0,h,-1))
+            x0 = x0 + 2*h
+        end do
+
+    end function trapezio
+
+    function simpson(N) result(res)
+        real(p) :: res, h
+        integer :: N, i
+        real(p) :: x0
+
+        res = 0.
+        h = find_h(N)
+        x0 = a + h
+
+        do i = 1, int(N/2)
+            res = res + h/3 * (fn(x0,h,1) + 4*fn(x0,h,0) + fn(x0,h,-1))
+            x0 = x0 + 2*h
+        end do
+    end function simpson
+    
+    function bode(N) result(res)
+        real(p) :: res, h
+        integer :: N, i
+        real(p) :: x0
+
+        res = 0.
+        h = (b-a)/N
+        x0 = a + 2*h
+
+        do i = 1, int(N/4)
+            res = res + 2*h/45 * (7*fn(x0,h,0) + 32*fn(x0,h,1) &
+              & + 12*fn(x0,h,2) + 32*fn(x0,h,3) + 7*fn(x0,h,4))
+            x0 = x0 + 4*h
+        end do
+
+    end function bode
+end module integrals
+
+module data_handler
+    use parametros
+    use integrals
+    use func
+    implicit none
+
+    contains
+
+    subroutine load_tab2_in(N_num, N_array)
+        integer, intent(out) :: N_num
+        integer, allocatable, intent(out) :: N_array(:)
+
+        open(unit=1, file= "tab2_in.dat", status ="old", action = "read" )
+        read(1, *) N_num
+
+        allocate(N_array(N_num))
+        read (1, *) N_array
+
+        close(1)
+    end subroutine load_tab2_in
+
+    subroutine gen_out(N_num, N_array)
+        integer, intent(in) :: N_num
+        integer, allocatable, intent(in) :: N_array(:)
+
+        real(p), allocatable :: matrix_out(:,:)
+        integer :: i, min_ind
+        real(p) :: true_val, min_val
+        integer :: min_ind_vec(1)
+
+        character(len = 50) :: label(5)
+        label(1) = "N"
+        label(2) = "h"
+        label(3) = "Regra do trapézio"
+        label(4) = "Regra de Simpson"
+        label(5) = "Regra de Bode"
+        
+        ! Derivada simétrica de 5 pontos não é usada msm?
+
+        allocate(matrix_out(N_num, 4))
+
+        do i = 1, N_num
+           matrix_out(i, 1) = find_h(N_array(i))
+           matrix_out(i, 2) = trapezio(N_array(i))
+           matrix_out(i, 3) = simpson(N_array(i))
+           matrix_out(i, 4) = bode(N_array(i))
+        end do
+
+        true_val = int_f()
+
+        ! Calcular diferença do valor teórico
+        matrix_out = transpose(matrix_out) ! h_num x 6 -> 6 x h_num
+        do i = 2, 4
+            matrix_out(i,:) = abs(matrix_out(i,:) - true_val) 
+        end do
+
+        print *, "Melhores valores de N: "
+        print *, ""
+
+        do i = 2, 4
+            min_val = minval(matrix_out(i, :))
+            min_ind_vec = minloc(matrix_out(i, :))
+            min_ind = min_ind_vec(1)
+
+            print *, trim(label(i+1)), " :"
+            write(*,"(A)",advance = "no") " N = "
+            write(*,*) N_array(min_ind) 
+            print *, "Diferença de ", min_val
+            print *, ""
+        end do
+
+        matrix_out = transpose(matrix_out)
+
+        open(unit=1, file="tab2_out.dat", status="replace", action = "write") 
+
+        do i = 1, 5
+            write(1, '(A, A)', advance='no') trim(label(i)), '   '
+        end do
+        write(1, *) 
+            
+        do i = 1, N_num
+            write(1,"(I5)", advance = "no") N_array(i)
+            write(1, *) matrix_out(i, :)
+        end do
+
+        close(1)
+
+        deallocate(matrix_out)
+    end subroutine gen_out
+
+end module data_handler
+
+program exer2
+    use parametros
+    use data_handler
+    implicit none
+
+    integer :: N_num
+    integer, allocatable:: N_array(:)
+
+    call load_tab2_in(N_num, N_array)
+    call gen_out(N_num, N_array)
+
+    deallocate(N_array)
+    
+end program exer2
