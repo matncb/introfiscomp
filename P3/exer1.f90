40be304fa7e63656298dd0786aee7c6cc69691c8
matheusncb@usp.br <a15479472@ametista1.lef>
2025-10-01 11:44:38 -0300

    Começando P3

--- P3/exer1.f90
+++ P3/exer1.f90
@@ -8,14 +8,60 @@ module precision
     public :: sp, dp, p
 end module precision
 
+module param
+    use precision
+    implicit none
+
+    real(p), parameter :: m = 80._p 
+    real(p),parameter :: P = 400._p
+
+    private
+    public :: m, P
+
+end module param
+
 
 module euler
     use precision
+    use param
     implicit none
     
 contains
+    subroutine solve(v0, T, delta_t, N, t_array, v_array)
+        real(p), intent(in) :: v0, T, delta_t
+        integer, intent(out) :: N
+        integer :: i
+        
+        real(p), allocatable, intent(out) :: v_array(:), t_array(:)
+    
+        N = int(T/delta_t) + 1
+
+        allocate(v_array(N))
+        allocate(t_array(N))
+
+        v_array(0) = v0
+        t_array(0) = 0._p
+
+        do i = 0, N-2
+            v_array(i+1) = v_array(i) + delta_t * P/(m*v_array(i))
+            t_array(i+1) = (i+1)*delta_t
+        end do
+    end subroutine solve
 
 end module euler
+
+module data_handler
+    use precision
+    use euler
+    implicit none
+
+contains
+
+    subroutine gen_out()
+       
+    end subroutine gen_out
+
+end module data_handler
 
 
 
